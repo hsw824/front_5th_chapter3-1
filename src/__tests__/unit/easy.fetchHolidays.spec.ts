eb160d3e48dd6a40e9b0976168249b1ef81bf7ed
hsw824 <ghdtjddn0824@naver.com>
2025-05-14 01:13:58 +0900

    test: fetchHolidays 테스트 추가

--- src/__tests__/unit/easy.fetchHolidays.spec.ts
+++ src/__tests__/unit/easy.fetchHolidays.spec.ts
@@ -1,9 +1,20 @@
 import { fetchHolidays } from '../../apis/fetchHolidays';
 
 describe('fetchHolidays', () => {
-  it('주어진 월의 공휴일만 반환한다', () => {});
+  it('주어진 월의 공휴일만 반환한다', () => {
+    expect(fetchHolidays(new Date(2025, 4, 1))).toEqual({ '2025-05-05': '어린이날' });
+  });
 
-  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {});
+  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {
+    expect(fetchHolidays(new Date(2025, 1, 1))).toEqual({});
+  });
 
-  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {});
+  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {
+    expect(fetchHolidays(new Date(2025, 0, 1))).toEqual({
+      '2025-01-01': '신정',
+      '2025-01-29': '설날',
+      '2025-01-30': '설날',
+      '2025-01-31': '설날',
+    });
+  });
 });
